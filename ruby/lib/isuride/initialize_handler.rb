fc0434e36da4f3912c2d32bb70e39c7bd3cf5fcd
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2025-01-02 17:16:59 +0900

    リトライ

--- ruby/lib/isuride/initialize_handler.rb
+++ ruby/lib/isuride/initialize_handler.rb
@@ -18,6 +18,31 @@ module Isuride
 
       db.xquery("UPDATE settings SET value = ? WHERE name = 'payment_gateway_url'", req.payment_server)
 
+      chairs = db.xquery('SELECT * FROM chairs')
+      chairs.each do |chair|
+        chair_id = chair.fetch(:id)
+        locations = db.xquery('SELECT * FROM chair_locations WHERE chair_id = ? ORDER BY created_at', chair_id)
+        next if locations.count.zero?
+        total_distance = 0
+        locations.each_cons(2) do |(a, b)|
+          total_distance += calculate_distance(
+            a.fetch(:latitude),
+            a.fetch(:longitude),
+            b.fetch(:latitude),
+            b.fetch(:longitude)
+          )
+        end
+        latset_location = db.xquery('SELECT * FROM chair_locations WHERE chair_id = ? ORDER BY created_at DESC LIMIT 1', chair_id).first
+        # 最新の位置情報をlatest_chair_locationsに保存
+        db.xquery(
+          'INSERT INTO latest_chair_locations (chair_id, latitude, longitude, total_distance) VALUES (?, ?, ?, ?)',
+          chair_id,
+          latset_location.fetch(:latitude),
+          latset_location.fetch(:longitude),
+          total_distance
+        )
+      end
+
       Thread.new do
         loop do
           begin
